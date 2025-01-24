ceb5b92bc6e73fb3a8692207408e3fa68090c1ed
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-01-24 10:03:53 +0300

    Configurate vite to run the project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import 'process'
 import react from '@vitejs/plugin-react'
+
+process.env.BROWSER = 'google-chrome';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: '/demo/',
+
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    open: 'index.html'
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: '8080',
+    strictPort: true,
+  }
 })
